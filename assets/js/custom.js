b9e8a5fe8f0fd833786c8517e1b56dc602bc5720
EslamEs1 <eslamdeveloper1@gmail.com>
2024-06-05 06:16:07 +0300

    d

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -231,90 +231,67 @@ document.addEventListener("DOMContentLoaded", function () {
 
 
 // Testimonials;
-$(document).ready(function () {
-    var sync1 = $("#sync1");
-    var sync2 = $("#sync2");
-    var slidesPerPage = 4; //globaly define number of elements per page
-    var syncedSecondary = true;
-
-    sync1
-        .on("initialized.owl.carousel", function () {
-            sync1
-                .find(".owl-video")
-                .eq(0)
-                .find(".owl-video-play-icon")
-                .trigger("click");
-        })
-        .owlCarousel({
+$(".owl-testimonials").owlCarousel({
+    stagePadding: 200,
+    loop: true,
+    margin: 10,
+    items: 1,
+    nav: true,
+    responsive: {
+        0: {
             items: 1,
-            dots: false,
-            nav: false,
-            loop: false,
-            video: true,
-        })
-        .on("changed.owl.carousel", syncPosition);
+            stagePadding: 60,
+        },
+        600: {
+            items: 1,
+            stagePadding: 100,
+        },
+        1000: {
+            items: 1,
+            stagePadding: 200,
+        },
+        1200: {
+            items: 1,
+            stagePadding: 250,
+        },
+        1400: {
+            items: 1,
+            stagePadding: 300,
+        },
+        1600: {
+            items: 1,
+            stagePadding: 350,
+        },
+        1800: {
+            items: 1,
+            stagePadding: 400,
+        },
+    },
+});
 
-    sync2
-        .on("initialized.owl.carousel", function () {
-            sync2.find(".owl-item").eq(0).addClass("current");
-        })
-        .owlCarousel({
-            items: slidesPerPage,
-            dots: false,
-            nav: false,
-            loop: false,
-            video: true,
-            slideBy: slidesPerPage, //alternatively you can slide by 1, this way the active slide will stick to the first item in the second carousel
-            responsiveRefreshRate: 100,
-        })
-        .on("changed.owl.carousel", syncPosition2);
+var playerSettings = {
+    controls: ["play-large"],
+    fullscreen: { enabled: false },
+    resetOnEnd: true,
+    hideControls: true,
+    clickToPlay: true,
+    keyboard: false,
+};
 
-    function syncPosition(el) {
-        //if you set loop to false, you have to restore this next line
-        //var current = el.item.index;
+const players = Plyr.setup(".js-player", playerSettings);
 
-        //if you disable loop you have to comment this block
-        var count = el.item.count - 1;
-        var current = Math.round(el.item.index - el.item.count / 2 - 0.5);
+players.forEach(function (instance, index) {
+    instance.on("play", function () {
+        players.forEach(function (instance1, index1) {
+            if (instance != instance1) {
+                instance1.pause();
+            }
+        });
+    });
+});
 
-        if (current < 0) {
-            current = count;
-        }
-        if (current > count) {
-            current = 0;
-        }
-
-        //end block
-
-        sync2
-            .find(".owl-item")
-            .removeClass("current")
-            .eq(current)
-            .addClass("current");
-        var onscreen = sync2.find(".owl-item.active").length - 1;
-        var start = sync2.find(".owl-item.active").first().index();
-        var end = sync2.find(".owl-item.active").last().index();
-
-        if (current > end) {
-            sync2.data("owl.carousel").to(current, 100, true);
-        }
-        if (current < start) {
-            sync2.data("owl.carousel").to(current - onscreen, 100, true);
-        }
-    }
-
-    function syncPosition2(el) {
-        if (syncedSecondary) {
-            var number = el.item.index;
-            sync1.data("owl.carousel").to(number, 100, true);
-        }
-    }
-
-    sync2.on("click", ".owl-item", function (e) {
-        e.preventDefault();
-        var number = $(this).index();
-        sync1.data("owl.carousel").to(number, 300, true);
-
-        //sync1.data('to.owl.carousel', [event.item.index,300,true]);
+$(".video-section").on("translated.owl.carousel", function (event) {
+    players.forEach(function (instance, index1) {
+        instance.pause();
     });
 });
